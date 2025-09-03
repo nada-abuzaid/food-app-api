ebfe3afe8df8ca309278a8b9bf115220440e97bc
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2025-09-03 10:26:25 +0300

    chore: add basics middleware

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+app.use([
+    cors(),
+    express.json(),
+    morgan('dev')
+])
 
 app.get('/', (_req, res) => {
   return res.status(200).send('<h1>Welcome to the Food Ordering API</h1>');
